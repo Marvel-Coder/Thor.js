3a61143d76351198b9e5d817b6bba3d92c402999
Marvel-Coder <90670265+Marvel-Coder@users.noreply.github.com>
2021-09-15 12:16:26 +0530

    Create fact.js

---
+++ Commands/Fun/fact.js
@@ -0,0 +1,19 @@
+const fetch = require("node-fetch");
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "fact",
+  description: "Tells you a random fact",
+  async execute(message, args, client) {
+    let res = await fetch(
+      "https://uselessfacts.jsph.pl/random.json?language=en"
+    );
+    let json = await res.json();
+    let text = json.text;
+    let safeText = text.replace(/`/g, "'");
+    let embed = new Discord.MessageEmbed()
+      .setColor("#3bffba")
+      .setDescription(safeText);
+    message.channel.send(embed);
+  }
+};
