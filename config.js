6422cf3f23b0dc55084597cf8c894259d0b62629
Marvel-Coder <90670265+Marvel-Coder@users.noreply.github.com>
2021-09-15 08:09:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.Prefix = `>`; //your prefix for bot
-exports.Token = `Your bot token here`; //your token
+exports.Token = `DQ2NjQ2MTE4NDAyMjMjM5.YKydpA.-boeHuaVBPFdOk9KGe-W-aD6lBs``; //your token
 exports.Color = `BLUE`; //color   of embed
